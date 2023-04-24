86dcd59442548b98b6e8ef8af5bfc1b96e2dcb67
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-04-23 23:34:01 -0700

    updated apod layout

--- src/pages/apod.tsx
+++ src/pages/apod.tsx
@@ -3,12 +3,32 @@ import React from "react";
 
 import styles from '../styles/pages/ApodPage.module.css'
 
+// export const getStaticProps = async () => {
+
+//   const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       apod: data
+//     }
+//   }
+// }
+
 
 const ApodPage: MyPage = (props) => {
   return (
     <>
       <section className={styles.container} >
-        apod
+
+        <div className={styles.image}>
+          image
+        </div>
+
+        <div className={styles.description}>
+          description
+        </div>
+
       </section>
     </>
   );

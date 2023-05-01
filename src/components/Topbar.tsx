a4b33770b4e22f9426ef06db878330beb47abfda
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-05-01 09:08:28 -0700

    updated css

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -4,7 +4,7 @@ export default function Topbar({ onToggle }: any) {
     return (
         <>
             <section className={styles.container}>
-                <div className={styles.title}>Topbar</div>
+                <div className={styles.title}>NASA Api Explorer</div>
                 <button className={styles.toggle} onClick={onToggle}>
                     Toggle
                 </button>

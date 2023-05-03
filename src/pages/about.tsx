7cc2da3821148cd3968c03cc36fc7f60faffad3c
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-05-03 11:05:18 -0700

    commit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,14 +5,61 @@ const AboutPage = () => {
     return (
         <>
             <section className={styles.container}>
-                <div>About</div>
-                <div>What is an Api?</div>
-                <div>Astronomy Picture of the Day</div>
-                <div>Image Library</div>
-                <div>Video Library</div>
-                <div>Earth</div>
-                <div>EPIC</div>
-                <div>Mars Rover Photos</div>
+                <div className={styles.title}>
+                    <h1>About</h1>
+                </div>
+                <div className={styles.explanation}>
+                    <h1>What is an Api?</h1>
+                    <h4>
+                        API stands for application programming interface, which
+                        is a set of definitions and protocols for building and
+                        integrating application software.
+                    </h4>
+                </div>
+                <div className={styles.apod}>
+                    <h1>Astronomy Picture of the Day</h1>{' '}
+                    <h4>
+                        Discover the cosmos! Each day a different image or
+                        photograph of our fascinating universe is featured,
+                        along with a brief explanation written by a professional
+                        astronomer.
+                    </h4>
+                </div>
+                <div className={styles.image}>
+                    <h1>Image Library</h1>{' '}
+                    <h4>Search NASA's official image database.</h4>
+                </div>
+                <div className={styles.video}>
+                    <h1>Video Library</h1>{' '}
+                    <h4>Search NASA's official video database.</h4>
+                </div>
+                <div className={styles.earth}>
+                    <h1>Earth</h1>
+                    <h4>
+                        Landsat imagery is provided to the public as a joint
+                        project between NASA and USGS.
+                    </h4>
+                </div>
+                <div className={styles.epic}>
+                    <h1>EPIC</h1>
+                    <h4>
+                        The EPIC API provides information on the daily imagery
+                        collected by DSCOVR's Earth Polychromatic Imaging Camera
+                        (EPIC) instrument. Uniquely positioned at the Earth-Sun
+                        Lagrange point, EPIC provides full disc imagery of the
+                        Earth and captures unique perspectives of certain
+                        astronomical events such as lunar transits using a
+                        2048x2048 pixel CCD (Charge Coupled Device) detector
+                        coupled to a 30-cm aperture Cassegrain telescope.
+                    </h4>
+                </div>
+                <div className={styles.mars}>
+                    <h1>Mars Rover Photos</h1>{' '}
+                    <h4>
+                        View the most recent image data gathered by NASA's
+                        Curiosity, Opportunity, and Spirit rovers on Mars.
+                    </h4>
+                </div>
             </section>
         </>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,35 @@ import styles from '../styles/pages/HomePage.module.css';
 const HomePage = () => {
     return (
         <section className={styles.container}>
-            <div>This website is under construction.</div>
-            <div>About this website</div>
-            <div>Technical Details</div>
-            <div>Technologies</div>
+            This website is under construction.
+            <div className={styles.heading}>
+                <h1>Explore NASA's Imagery and Data</h1>
+                <h3>
+                    A free and open source project, built with public APIs
+                    provided by api.nasa.gov
+                </h3>
+                <button>Explore</button>
+            </div>
+            <div className={styles.about}>
+                <h1>About this website</h1>
+                <h3>
+                    This website was created as a hobby project to provide a
+                    front-end visiualition of the data provided by NASA's public
+                    API catalog.
+                </h3>
+            </div>
+            <div className={styles.details}>
+                <h1>Technical Details</h1>
+                <h3>
+                    The NASA API Explorer Website consists of a Next.js React
+                    application. It is written in Typescript and hosted by
+                    Vercel.
+                </h3>
+            </div>
+            <div className={styles.tech}>
+                <h1>Technologies</h1>
+                <div>Node.js React Next.js Typescript Vercel Jetbrains </div>
+            </div>
         </section>
     );
 };

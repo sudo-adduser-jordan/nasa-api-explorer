7a6157760fc34757b24279e06642ca4dc774954c
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-05-09 21:28:12 -0700

    validate metadata: passed

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -55,6 +55,61 @@ test.describe('Validate Routes', () => {
     });
 });
 
+// METADATA
+test.describe('Validate Metadata', () => {
+    test('Test Home Page', async ({ page }) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('link', { name: 'Home' }).click();
+        await expect(page).toHaveTitle(/Nasa Api Explorer/);
+    });
+    test('Test About Page', async ({ page }) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('link', { name: 'About' }).click();
+        await expect(page).toHaveTitle(/About/);
+        await expect(page).toHaveURL(/about/);
+    });
+
+    test('Test Apod Page', async ({ page }) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('link', { name: 'APOD' }).click();
+        await expect(page).toHaveTitle(/Astronomy Picture of the Day/);
+        await expect(page).toHaveURL(/apod/);
+    });
+
+    test('Test Image Library Page', async ({ page }) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('link', { name: 'Image Library' }).click();
+        await expect(page).toHaveTitle(/Image Library/);
+        await expect(page).toHaveURL(/image-library/);
+    });
+
+    test('Test Video Library Page', async ({ page }) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('link', { name: 'Video Library' }).click();
+        await expect(page).toHaveTitle(/Video Library/);
+        await expect(page).toHaveURL(/video-library/);
+    });
+
+    test('Test Mars Rover Photos Pages', async ({ page }) => {
+        await page.goto('http://localhost:3000/');
+        await page.getByRole('link', { name: 'Mars Rover Photos' }).click();
+        await expect(page).toHaveTitle(/Spirit/);
+        await expect(page).toHaveURL(/mars-rover-photos\/spirit/);
+
+        await page.getByRole('link', { name: 'curiosity' }).click();
+        await expect(page).toHaveTitle(/Curiosity/);
+        await expect(page).toHaveURL(/curiosity/);
+
+        await page.getByRole('link', { name: 'spirit' }).click();
+        await expect(page).toHaveTitle(/Spirit/);
+        await expect(page).toHaveURL(/spirit/);
+
+        await page.getByRole('link', { name: 'opportunity' }).click();
+        await expect(page).toHaveTitle(/Opportunity/);
+        await expect(page).toHaveURL(/opportunity/);
+    });
+});
+
 // EXTERNAL LINKS
 test.describe('Validate External Links', () => {
     test('Test Github Code Link', async ({ page }) => {
@@ -76,17 +131,6 @@ test.describe('Validate External Links', () => {
         const page1 = await page1Promise;
         await expect(page1).toHaveURL('https://github.com/sudo-adduser-jordan');
     });
-});
-
-// METADATA
-test.describe('Validate Metadata', () => {
-    test('Test Apod Data', async ({ page }) => {});
-
-    test('Test Image Library Data', async ({ page }) => {});
-
-    test('Test Video Library Data', async ({ page }) => {});
-
-    test('Test Mars Rover Photos Data', async ({ page }) => {});
 });
 
 // SERVER COMPONENTS

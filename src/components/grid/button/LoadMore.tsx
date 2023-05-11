95d9ba6847039ac860cca1ecbea77aaa23717def
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-05-11 14:59:45 -0700

    Update LoadMore.tsx

--- src/components/grid/button/LoadMore.tsx
+++ src/components/grid/button/LoadMore.tsx
@@ -3,7 +3,11 @@ import styles from './loadmore.module.css';
 function LoadButton({ loadMoreCards }: any) {
     return (
         <div className={styles.buttonContainer}>
-            <button className={styles.load} onClick={loadMoreCards}>
+            <button
+                className={styles.load}
+                onTouchStart={loadMoreCards}
+                onClick={loadMoreCards}
+            >
                 Load More
             </button>
         </div>

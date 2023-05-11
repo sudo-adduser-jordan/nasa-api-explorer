3a9f6ffe7b832b97a0931dba7e6a5c2d967552b5
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-05-11 12:46:37 -0700

    Update getApod.tsx

--- src/lib/apod/getApod.tsx
+++ src/lib/apod/getApod.tsx
@@ -1,7 +1,7 @@
 const defaultEndpoint = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
 
 export default async function getApod() {
-    const res = await fetch(defaultEndpoint, { next: { revalidate: 10 } });
+    const res = await fetch(defaultEndpoint, { next: { revalidate: 14400 } });
     if (!res.ok) throw new Error('Failed to fetch APOD Properties.');
     return res.json();
 }

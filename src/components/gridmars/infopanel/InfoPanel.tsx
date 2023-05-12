15dfa1ad7f7886f22d0aa7c14f1acd76a4a03696
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-05-12 11:25:18 -0700

    update css

--- src/components/gridmars/infopanel/InfoPanel.tsx
+++ src/components/gridmars/infopanel/InfoPanel.tsx
@@ -21,13 +21,42 @@ type Data = {
 function InfoPanel({ data }: Data) {
     const { name, max_sol, status, max_date, total_photos, launch_date } =
         data.manifest.photo_manifest;
+
+    const status_style = '';
+
     return (
-        <div className={styles.container}>
-            <h5 className={styles.status}>Status: {status}</h5>
-            <h5 className={styles.info}>Mars Days: {max_sol}</h5>
-            <h5 className={styles.info}>Total Photos: {total_photos}</h5>
-            <h5 className={styles.info}>Lanuch Date: {launch_date}</h5>
-            <h5 className={styles.info}>Max Earth Date: {max_date}</h5>
+        <div
+            className={
+                status === 'active' ? `${styles.active}` : `${styles.container}`
+            }
+        >
+            <h5 className={styles.info}>
+                <span className={styles.purple}>Status: </span>
+                <span
+                    className={
+                        status === 'active'
+                            ? `${styles.green}`
+                            : `${styles.red}`
+                    }
+                >
+                    {status}
+                </span>
+            </h5>
+            <h5 className={styles.info}>
+                <span className={styles.purple}>Lanuch Date:</span>{' '}
+                {launch_date}
+            </h5>
+            <h5 className={styles.info}>
+                <span className={styles.purple}>Max Earth Date:</span>{' '}
+                {max_date}
+            </h5>
+            <h5 className={styles.info}>
+                <span className={styles.purple}>Mars Days:</span> {max_sol}
+            </h5>
+            <h5 className={styles.info}>
+                <span className={styles.purple}>Total Photos:</span>{' '}
+                {total_photos}
+            </h5>
         </div>
     );
 }

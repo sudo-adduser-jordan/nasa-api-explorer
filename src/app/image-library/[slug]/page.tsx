02f192e1524966dfe49e9c358bfc044e2d8b9133
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-05-14 23:31:08 -0700

    update tags

--- src/app/image-library/[slug]/page.tsx
+++ src/app/image-library/[slug]/page.tsx
@@ -1,22 +1,22 @@
 import styles from './imageslug.module.css';
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-    title: 'Image Library',
-    description: 'Image Library Page',
-};
+// export const metadata: Metadata = {
+//     title: 'Image Library',
+//     description: 'Image Library Page',
+// };
 
 async function Page({ params }: { params: { slug: string } }) {
     const content = (
-        <section className={styles.container}>
+        <div className={styles.container}>
             <div className={styles.title}>
                 {params.slug.replace(/%20/g, ' ')}
             </div>
-            <section className={styles.content}>
+            <div className={styles.content}>
                 <div className={styles.image}>image</div>
                 <div className={styles.description}>description</div>
-            </section>
-        </section>
+            </div>
+        </div>
     );
 
     return content;
--- src/app/video-library/[slug]/page.tsx
+++ src/app/video-library/[slug]/page.tsx
@@ -1,22 +1,22 @@
 import { Metadata } from 'next';
 import styles from './videoslug.module.css';
 
-export const metadata: Metadata = {
-    title: 'Video Library',
-    description: 'Video Library Page',
-};
+// export const metadata: Metadata = {
+//     title: 'Video Library',
+//     description: 'Video Library Page',
+// };
 
 async function Page({ params }: { params: { slug: string } }) {
     const content = (
-        <section className={styles.container}>
+        <div className={styles.container}>
             <div className={styles.title}>
                 {params.slug.replace(/%20/g, ' ')}
             </div>
-            <section className={styles.content}>
+            <div className={styles.content}>
                 <div className={styles.image}>video</div>
                 <div className={styles.description}>description</div>
-            </section>
-        </section>
+            </div>
+        </div>
     );
     return content;
 }

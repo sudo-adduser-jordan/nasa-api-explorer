805e3e5fae8594071615e998f65f8d1eb13ad169
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-08-20 06:51:36 -0700

    Refactor: Button CSS
    -update card button css

--- src/components/gridmars/card/MarsCard.tsx
+++ src/components/gridmars/card/MarsCard.tsx
@@ -1,31 +1,31 @@
-import styles from './marscard.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
+import styles from "./marscard.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export type MarsCard = {
-    href: string;
-    date: string;
-    sol: number;
+  href: string;
+  date: string;
+  sol: number;
 };
 
 function MarsCard({ href, date, sol }: MarsCard) {
-    return (
-        <div className={styles.card}>
-            <div className={styles.image}>
-                <Image src={href} width={250} height={250} alt='' />
-            </div>
-            <div className={styles.date}>
-                <span style={{ color: 'tomato' }}>Date: </span>
-                {date}
-            </div>
-            <div className={styles.sol}>
-                <span style={{ color: 'tomato' }}>Sol: </span> {sol}
-            </div>
-            <Link className={styles.details} href={`/`}>
-                Details &rarr;
-            </Link>
-        </div>
-    );
+  return (
+    <div className={styles.card}>
+      <div className={styles.image}>
+        <Image src={href} width={250} height={250} alt="" />
+      </div>
+      <div className={styles.date}>
+        <span style={{ color: "tomato" }}>Date: </span>
+        {date}
+      </div>
+      <div className={styles.sol}>
+        <span style={{ color: "tomato" }}>Sol: </span> {sol}
+      </div>
+      <button className={styles.button}>
+        <Link href={`/`}>Details &rarr;</Link>
+      </button>
+    </div>
+  );
 }
 
 export default MarsCard;
